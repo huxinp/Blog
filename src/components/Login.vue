b319ae44709dab2ac4d4cbc6f51b935e5ab258a7
胡新鹏 <752656520@qq.com>
2017-06-20 23:23:02 +0800

    提交

---
+++ src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+	<div></div>
+</template>
+<script type="text/ecmascript-6">
+	export default {
+		data(){
+			return {
+
+			}
+		},
+		prop: {
+
+		},
+		created(){
+			
+		}
+	}
+</script>
+<style lang='scss'>
+	
+</style>
